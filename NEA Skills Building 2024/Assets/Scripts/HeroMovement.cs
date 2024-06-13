c7cea69a2778f9b03b00797bb5f0ba3f819190f5
GrigelsE01 <grigels.e01@elliotthudsoncollege.ac.uk>
2024-06-13 12:09:03 +0100

    End of Task 4

--- NEA Skills Building 2024/Assets/Scripts/HeroMovement.cs
+++ NEA Skills Building 2024/Assets/Scripts/HeroMovement.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeroMovement : MonoBehaviour {
 
 	Rigidbody2D rb;
 	float speed;
+	public int lives;
+	bool vulnerable;
 	// Use this for initialization
+
+	public void setLives(){
+		lives -= 1;
+		if (lives <= 0 || vulnerable == true) {
+			Debug.Log ("End of game");
+			SceneManager.LoadScene ("Lost");
+		}
+	}
+
 	void Start () {
 		speed = 5.1f;
+		lives = 2;
+		vulnerable = false;
 		rb = GetComponent<Rigidbody2D>();
 	}
 	
@@ -23,9 +37,28 @@ public class HeroMovement : MonoBehaviour {
 		rb.AddForce(Vector2.up * 300);
 	}
 }
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "extraLife") {
+			lives += 1;
+			Destroy (other.gameObject);
+		} else if (other.gameObject.tag == "vulnerable") {
+			Destroy (other.gameObject);
+			vulnerable = true;
+			Debug.Log ("vulnerable = true!");
+			StartCoroutine ("VulnerableDeBuff");
+		}
+	}
+
+	IEnumerator VulnerableDeBuff()
+	{
+		yield return new WaitForSeconds (5f);
+		vulnerable = false;
+		Debug.Log ("vulnerable = false!");
+	}
+
 	void resetPosition(){
 		Debug.Log ("SPIKED RECEIVED!");
 		transform.SetPositionAndRotation (new Vector3 (-5.58f, 1.34f, 0), Quaternion.identity);
-
+		setLives ();
 	}
 }
