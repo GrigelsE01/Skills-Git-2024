a0ab755f47d21f13c5ba7f855f27e53cd1f458b7
GrigelsE01 <grigels.e01@elliotthudsoncollege.ac.uk>
2024-06-18 11:36:19 +0100

    End of Task 12

---
+++ NEA Skills Building 2024/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawner : MonoBehaviour {
+	public GameObject bomb;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawner", 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawner(){
+		Instantiate (bomb, new Vector3(5, 0, 0), Quaternion.identity);
+	}
+}
